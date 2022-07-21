f1cee93c0ba7a4595d5de5be52f9f939349adcb3
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-07-21 11:39:58 -0700

    first commit

---
+++ ProductManager/server.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const express = require('express');
+const cors = require('cors');
+const port = 8000;
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('./server/config/mongoose.config');
+require('./server/routes/product.routes')(app);
+
+app.listen(port, () => console.log(`You are now listening to port: ${port}`));
